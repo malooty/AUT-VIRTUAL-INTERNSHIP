940a7573f23c162780a0073f0584a39818c2e32e
MALAVIKA SMITHA <101200371+malooty@users.noreply.github.com>
2024-06-27 09:56:14 +1200

    routes.py

--- routes.py
+++ routes.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 @main.route('/data', methods=['GET'])
 def get_data():
     try:
-        df = pd.read_csv('data/team_name1_price_fcst.csv')
+        df = pd.read_csv('data/team_name1_price_fcst_with_actual.csv')
         data = df.to_dict(orient='records')
         return jsonify(data)
     except Exception as e:
@@ -16,40 +16,17 @@ def get_data():
 def get_filtered_data():
     try:
         filters = request.get_json()
-        df = pd.read_csv('data/team_name1_price_fcst.csv')
+        df = pd.read_csv('data/team_name1_price_fcst_with_actual.csv')
+        
+        # Ensure date is treated as string
+        df['date'] = df['date'].astype(str)
         
         if 'date_range' in filters:
             start_date, end_date = filters['date_range']
             df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
         
-        if 'series' in filters:
-            series = filters['series']
-            if not isinstance(series, list):
-                series = [series]
-            df = df[df['series'].isin(series)]
-        
         data = df.to_dict(orient='records')
         return jsonify(data)
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-@main.route('/statistics', methods=['POST'])
-def calculate_statistics():
-    try:
-        data = request.get_json()
-        df = pd.DataFrame(data)
-        
-        if 'actual_price' in df.columns:
-            grouped = df.groupby('date')
-            rmse_results = {}
-
-            for name, group in grouped:
-                rmse = ((group['actual_price'] - group['price_fcst']) ** 2).mean() ** 0.5
-                rmse_results[name] = rmse
-            
-            return jsonify(rmse_results)
-        else:
-            return jsonify({"error": "The provided data does not include 'actual_price' column."})
     except Exception as e:
         return jsonify({"error": str(e)})
 
@@ -57,17 +34,14 @@ def calculate_statistics():
 def calculate_filtered_statistics():
     try:
         filters = request.get_json()
-        df = pd.read_csv('data/team_name1_price_fcst.csv')
+        df = pd.read_csv('data/team_name1_price_fcst_with_actual.csv')
+        
+        # Ensure date is treated as string
+        df['date'] = df['date'].astype(str)
         
         if 'date_range' in filters:
             start_date, end_date = filters['date_range']
             df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
-        
-        if 'series' in filters:
-            series = filters['series']
-            if not isinstance(series, list):
-                series = [series]
-            df = df[df['series'].isin(series)]
         
         if 'actual_price' in df.columns:
             grouped = df.groupby('date')
